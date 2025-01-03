4cd6ebc0d7aa54b46748ab29cc6a6d54ea275be7
magartrantit <nazacser@gmail.com>
2025-01-03 21:24:10 +0200

    modificari la marimea fructelor si sarpelui

--- Proiect/snake.py
+++ Proiect/snake.py
@@ -7,6 +7,7 @@ black = pygame.Color(0, 0, 0)
 white = pygame.Color(255, 255, 255)
 red = pygame.Color(213, 50, 80)
 green = pygame.Color(0, 255, 0)
+blue = pygame.Color(50, 153, 213)
 
 #game variables
 window_x = 720
@@ -17,15 +18,15 @@ game_window = pygame.display.set_mode((window_x, window_y))
 fps = pygame.time.Clock()
 
 #snake
-snake_pos = [100, 50]
-snake_body = [ [100, 50], [90, 50], [80, 50], [70, 50]]
+snake_pos = [120, 80]
+snake_body = [[120, 80], [100, 60], [80, 40], [60, 20]]
 snake_speed = 15
 direction = 'RIGHT'
 change_to = direction
 
 #fruit
 fruit_spawn = True
-fruit_pos = [random.randrange(1, (window_x//10)) * 10, random.randrange(1, (window_y//10)) * 10]
+fruit_pos = [random.randrange(0, (window_x//20)) * 20, random.randrange(0, (window_y//20)) * 20]
 
 #score
 score = 0
@@ -37,14 +38,15 @@ def show_score(choice, color, font, size):
 
 def set_game():
     global snake_pos, snake_body, snake_speed, direction, change_to, fruit_pos, fruit_spawn, score
-    snake_pos = [100, 50]
-    snake_body = [ [100, 50], [90, 50], [80, 50], [70, 50]]
+    snake_pos = [120, 80]
+    snake_body = [[120, 80], [100, 60], [80, 40], [60, 20]]
     snake_speed = 15
     direction = 'RIGHT'
     change_to = direction
     fruit_spawn = True
-    fruit_pos = [random.randrange(1, (window_x//10)) * 10, random.randrange(1, (window_y//10)) * 10]
+    fruit_pos = [random.randrange(0, (window_x//20)) * 20, random.randrange(0, (window_y//20)) * 20]
     score = 0
+
 
 def gmae_over():
 
@@ -109,13 +111,13 @@ def main():
             direction = 'RIGHT'
 
         if direction == 'UP':
-            snake_pos[1] -= 10
+            snake_pos[1] -= 20
         if direction == 'DOWN':
-            snake_pos[1] += 10
+            snake_pos[1] += 20
         if direction == 'LEFT':
-            snake_pos[0] -= 10
+            snake_pos[0] -= 20
         if direction == 'RIGHT':
-            snake_pos[0] += 10
+            snake_pos[0] += 20
 
         snake_body.insert(0, list(snake_pos))
         if snake_pos[0] == fruit_pos[0] and snake_pos[1] == fruit_pos[1]:
@@ -126,15 +128,15 @@ def main():
         
 
         if not fruit_spawn:
-            fruit_pos = [random.randrange(1, (window_x//10)) * 10, random.randrange(1, (window_y//10)) * 10]
+            fruit_pos = [random.randrange(0, (window_x//20)) * 20, random.randrange(0, (window_y//20)) * 20]
 
         fruit_spawn = True
         game_window.fill(black)
 
         for pos in snake_body:
-            pygame.draw.rect(game_window, green, pygame.Rect(pos[0], pos[1], 10, 10))
+            pygame.draw.rect(game_window, blue, pygame.Rect(pos[0], pos[1], 20, 20))
         
-        pygame.draw.rect(game_window, white, pygame.Rect(fruit_pos[0], fruit_pos[1], 10, 10))
+        pygame.draw.rect(game_window, white, pygame.Rect(fruit_pos[0], fruit_pos[1], 20, 20))
 
         if snake_pos[0] < 0 or snake_pos[0] > window_x-10:
             gmae_over()
